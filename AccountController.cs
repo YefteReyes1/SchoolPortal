9b5ee6098e36dd2d623b6204fe11f4d01767e00e
Yefte Reyes Vargas <127820782+YefteReyes1@users.noreply.github.com>
2025-02-28 00:25:03 -0700

    Add files via upload

---
+++ AccountController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+public class AccountController : Controller
+{
+    [HttpGet]
+    public IActionResult Login()
+    {
+        return Challenge(new AuthenticationProperties { RedirectUri = "/" }, OpenIdConnectDefaults.AuthenticationScheme);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        return RedirectToAction("Index", "Home");
+    }
+}
